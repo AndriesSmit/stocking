5f2488c19ea609551d50e1e59e91d714e18938ee
AndriesSmit <gemsbokmuis@gmail.com>
2022-07-22 10:09:20 -0700

    installed devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,26 @@
 class HomeController < ApplicationController
+  
   def index
+        
+        @api = StockQuote::Stock.new(api_key: 'pk_51ca322c84af4737b6f81261e456a66d')
+
+        if params[:ticker] == ""
+            @nothing = "Hey! You forgot To Enter A Symbol"
+        elsif  
+              if params[:ticker]
+                begin
+                @stock_logo = StockQuote::Stock.logo(params[:ticker])
+                @stock = StockQuote::Stock.quote(params[:ticker])
+                rescue StandardError
+                  @error = "Hey! That Stock Symbol does not Exist. Please try again."
+                end
+
+              end
+        end
+    
   end
+
+  def about
+  end
+  
 end
